0af0c8c3be4f01e2633bf0f4462ae79ff17b41ad
naseeftp <naseeftp35@gmail.com>
2025-07-26 19:39:28 +0530

    doubley ll deletefront

---
+++ doubleylist.js/deleteFront.js
@@ -0,0 +1,73 @@
+class Node
+{
+    constructor(value)
+    {
+        this.value=value;
+        this.next=null;
+        this.prev=null
+    }
+}
+class DoubleyLinkedList
+{
+    constructor()
+    {
+        this.head=null;
+        this.tail=null;
+        this.size=0
+    }
+      prepend(value)
+  {
+       const newNode=new Node(value);
+       if(!this.head)
+       {
+           this.head=newNode;
+           this.tail=newNode;
+       }
+       else
+       {
+           newNode.next=this.head;
+           this.head.prev=newNode;
+           this.head=newNode;
+       }
+  }
+  deleteFromFront()
+  {
+      if(!this.head)
+      {
+          return null;
+      }
+      if(this.head==this.tail)
+      {
+          this.head=null
+          this.tail=null;
+      }
+      else
+      {
+          this.head=this.head.next;
+          this.head.prev=null;
+      }
+  }
+  
+
+  
+  print()
+  {
+     let  current=this.head;
+      let values=[]
+      while(current)
+      {
+          values.push(current.value)
+          current= current.next;
+      }
+      console.log('forward',values.join('<->'))
+  }
+}
+
+const list =new DoubleyLinkedList()
+list.prepend(19)
+list.prepend(17)
+list.prepend(15)
+list.prepend(13)
+list.prepend(12)
+ list.deleteFromFront()
+list.print()
